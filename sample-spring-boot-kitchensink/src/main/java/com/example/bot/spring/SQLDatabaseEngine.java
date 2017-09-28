6cb4c11ef915215c299c134f869635dea751f444
Ron Lee <ron.leelch@gmail.com>
2017-09-29 00:57:39 +0800

    lab3

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -12,7 +12,25 @@ public class SQLDatabaseEngine extends DatabaseEngine {
 	@Override
 	String search(String text) throws Exception {
 		//Write your code here
-		return null;
+		Connection connection = getConnection();
+		String result = null;
+		
+		PreparedStatement stmt = connection.prepareStatement(
+				"SELECT response FROM line WHERE keyword like concat('%', ?, '%')"
+				);
+		
+		stmt.setString(1, text);
+		ResultSet rs = stmt.executeQuery();
+		if (rs.next()==true) {
+			result = rs.getString(1);
+		}
+		rs.close();
+		stmt.close();
+		connection.close();
+		
+		if (result != null)
+			return result;
+		throw new Exception("NOT FOUND");
 	}
 	
 	
